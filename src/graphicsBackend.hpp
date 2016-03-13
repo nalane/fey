2d19b250b7d0d0a93c6f54e6f7d03583475f4fff
nalane <nalane@mymail.mines.edu>
2016-03-12 17:17:02 -0700

    Added camera to the graphicsBackend

--- src/graphicsBackend.hpp
+++ src/graphicsBackend.hpp
@@ -10,16 +10,20 @@
 class graphicsBackend; // Resolves circular class dependencies
 #include "engine.hpp"
 #include "shaderProgram.hpp"
+#include "camera.hpp"
 
 class graphicsBackend {
 private:
   GLFWwindow* window;
   shaderProgram shaderProg;
   GLuint vao;
+  camera* currentCamera;
 
 public:
   graphicsBackend(int w, int h, std::string windowTitle, std::string vertexSource, std::string fragmentSource);
   ~graphicsBackend();
 
   void drawToGraphics(engine* parentEngine);
+  
+  void setCamera(camera* c) : currentCamera(c) {};
 };
