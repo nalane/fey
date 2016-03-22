57468b5e251c9fe06d41500facdb20e8d4c466da
nalane <nalane@mymail.mines.edu>
2016-03-21 23:19:07 -0600

    Demo uses more complex model

--- src/object.cpp
+++ src/object.cpp
@@ -19,6 +19,8 @@ object::object(vector<glm::vec3> positions, float colors[4][4]) {
   //m.setVertices(positions);
   //m.addData(GL_ARRAY_BUFFER, 16 * sizeof(float), colors, GL_STATIC_DRAW, 1);
   m = loadFeyModel(getFullPath("feyData/library/cube.fey.fey.model"));
+
+  modelMatrix = glm::rotate(glm::radians(-90.0f), glm::vec3(1.0, 0.0, 0.0)) * modelMatrix;
 }
 
 // Removes "object" from GPU
@@ -28,6 +30,6 @@ object::~object() {
 
 // Tells GPU to render "object"
 void object::draw() {
-  modelMatrix = modelMatrix * glm::rotate(glm::radians(0.2f), glm::vec3(0.0, 1.0, 0.0));
+  modelMatrix = glm::rotate(glm::radians(0.2f), glm::vec3(0.0, 1.0, 0.0)) * modelMatrix;
   m->draw();
 }
