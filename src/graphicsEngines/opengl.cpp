ba5576c658f0998b0c1986653759c4776bd9b9a6
nalane <nathaniel.patrick.lane@gmail.com>
2018-03-26 15:48:28 -0600

    Pushing textures and uniforms to opengl shaders

--- src/graphicsEngines/opengl.cpp
+++ src/graphicsEngines/opengl.cpp
@@ -7,8 +7,8 @@ opengl::~opengl() {
 
 void opengl::glfwHints() {
     // Force use of OpenGL core
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     glfwWindowHint(GLFW_SAMPLES, numAASamples);
--- src/resources/glModel.cpp
+++ src/resources/glModel.cpp
@@ -7,12 +7,6 @@ using namespace std;
 glModel::glModel(const string& name) : model(name) {
     glGenVertexArrays(1, &vao);
     glBindVertexArray(vao);
-
-    // Bind uniforms to shader
-    glGenBuffers(1, &ubo);
-    glBindBuffer(GL_UNIFORM_BUFFER, ubo);
-    glBufferData(GL_UNIFORM_BUFFER, sizeof(modelUniforms), nullptr, GL_DYNAMIC_DRAW);
-    glBindBufferBase(GL_UNIFORM_BUFFER, 0, ubo);
 }
 
 // Removes all vbos and the vao
@@ -23,6 +17,9 @@ glModel::~glModel() {
 }
 
 void glModel::bindData() {
+    glShaderProgram* prog = (glShaderProgram*)child_resources["shaderProgs"]["default"];
+    prog->useProgram();
+
     glBindVertexArray(vao);
 
     glGenBuffers(1, &vbo);
@@ -30,6 +27,14 @@ void glModel::bindData() {
     glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(modelVertex), vertices.data(), GL_STATIC_DRAW);
 
     modelVertex::bindLayout();
+
+    // Bind uniforms to shader
+    glGenBuffers(1, &ubo);
+    glBindBuffer(GL_UNIFORM_BUFFER, ubo);
+    GLuint uniformIndex = glGetUniformBlockIndex(prog->getProgID(), "DefaultUniforms");
+    glBindBufferBase(GL_UNIFORM_BUFFER, 0, ubo);
+    glUniformBlockBinding(prog->getProgID(), uniformIndex, 0);
+    glBufferData(GL_UNIFORM_BUFFER, sizeof(modelUniforms), nullptr, GL_DYNAMIC_DRAW);
 }
 
 void glModel::draw(modelUniforms uniforms) {
@@ -40,8 +45,14 @@ void glModel::draw(modelUniforms uniforms) {
     glBindBuffer(GL_UNIFORM_BUFFER, ubo);
     glBufferSubData(GL_UNIFORM_BUFFER, 0, sizeof(uniforms), &uniforms);
 
-    // Bind textures
+    // Bind texture to uniform
     int index = 0;
+    GLuint texHandle = glGetUniformLocation(prog->getProgID(), "texSampler");
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, texHandle);
+    glUniform1i(texHandle, 0);
+
+    // Draw textures
     for (auto p : child_resources["textures"]) {
         glTexture* tex = (glTexture*)(p.second);
         tex->draw(index);
